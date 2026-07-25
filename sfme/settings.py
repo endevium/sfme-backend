8d29287718fc88cdf99dc90f1daa87cde36714d6
John Rasheed Paner <jrasheedpaner@gmail.com>
2026-07-25 20:54:52 +0800

    Fix ALLOWED_HOSTS

--- sfme/settings.py
+++ sfme/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
